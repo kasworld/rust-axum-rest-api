2332112ad61760f6c4b68194f49b06d5b5b0089f
seukwonkang <kasworld@gmail.com>
2026-09-14 15:23:00 +0900

    code reorder

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,6 @@ use serde::{Serialize,Deserialize};
 use serde_json;
 
 #[derive(Serialize, Deserialize)]
-struct Document {
-    id: i64,
-    user_id: Option<i64>,
-    title: String,
-    body: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct CreateDocument {
-    title: String,
-    body: String,
-    user_id: Option<i64>,
-}
-
- #[derive(Serialize, Deserialize)]
-struct UpdateDocument {
-    title: String,
-    body: String,
-    user_id: Option<i64>,
-}
-
-#[derive(Serialize, Deserialize)]
 struct User {
     id: i64,
     username: String,
@@ -40,45 +18,31 @@ struct CreateUser {
     username: String,
     email: String,
 }
- 
-#[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    // initialize tracing for logging
-    tracing_subscriber::fmt()
-        .with_max_level(Level::INFO)
-        .init();
- 
-    dotenv().ok();
-    let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = SqlitePoolOptions::new().connect(&url).await?;
-    info!("Connected to the database!");
- 
-    // build our application with a route
-    let app = Router::new()
-    .route("/users", post(create_user))
-    .route("/docs", get(get_document_list).post(create_document))
-    .route("/docs/{id}", get(get_document).put(update_document).delete(delete_document))
-    .layer(Extension(pool));
+async fn create_user(
+    Extension(pool): Extension<SqlitePool>,
+    Json(new_user): Json<CreateUser>,
+) -> Result<Json<User>, StatusCode> {
+    let user = sqlx::query_as!(
+        User,
+        "INSERT INTO users (username, email) VALUES ($1, $2) RETURNING id, username, email",
+        new_user.username,
+        new_user.email
+    )
+    .fetch_one(&pool)
+    .await
+    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    // run our app with hyper, listening globally on port 5000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
-    info!("Server is running on http://0.0.0.0:5000");
-    axum::serve(listener, app).await.unwrap();
- 
-    Ok(())
-}
- 
-async fn get_document_list(
-    Extension(pool): Extension<SqlitePool>
-) -> Result<Json<Vec<Document>>, StatusCode> {
-    let posts = sqlx::query_as!(Document, "SELECT id, user_id, title, body FROM documents")
-        .fetch_all(&pool)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
- 
-    Ok(Json(posts))
+    Ok(Json(user))
 }
 
+
+#[derive(Serialize, Deserialize)]
+struct Document {
+    id: i64,
+    user_id: Option<i64>,
+    title: String,
+    body: String,
+}
 async fn get_document(
     Extension(pool): Extension<SqlitePool>,
     Path(id): Path<i64>,
@@ -91,11 +55,41 @@ async fn get_document(
     .fetch_one(&pool)
     .await
     .map_err(|_| StatusCode::NOT_FOUND)?;
- 
+
     Ok(Json(doc))
 }
+async fn get_document_list(
+    Extension(pool): Extension<SqlitePool>
+) -> Result<Json<Vec<Document>>, StatusCode> {
+    let posts = sqlx::query_as!(Document, "SELECT id, user_id, title, body FROM documents")
+        .fetch_all(&pool)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
- 
+    Ok(Json(posts))
+}
+async fn delete_document(
+    Extension(pool): Extension<SqlitePool>,
+    Path(id): Path<i64>,
+) -> Result<Json<serde_json::Value>, StatusCode> {
+    let result = sqlx::query!("DELETE FROM documents WHERE id = $1", id)
+        .execute(&pool)
+        .await;
+
+    match result {
+        Ok(_) => Ok(Json(serde_json::json! ({
+            "message": "Document deleted successfully"
+        }))),
+        Err(_) => Err(StatusCode::NOT_FOUND),
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct CreateDocument {
+    title: String,
+    body: String,
+    user_id: Option<i64>,
+}
 async fn create_document(
     Extension(pool): Extension<SqlitePool>,
     Json(new_doc): Json<CreateDocument>,
@@ -110,11 +104,16 @@ async fn create_document(
     .fetch_one(&pool)
     .await
     .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
- 
+
     Ok(Json(doc))
 }
 
- 
+ #[derive(Serialize, Deserialize)]
+struct UpdateDocument {
+    title: String,
+    body: String,
+    user_id: Option<i64>,
+}
 async fn update_document(
     Extension(pool): Extension<SqlitePool>,
     Path(id): Path<i64>,
@@ -130,7 +129,7 @@ async fn update_document(
     )
     .fetch_one(&pool)
     .await;
- 
+
     match doc {
         Ok(post) => Ok(Json(post)),
         Err(_) => Err(StatusCode::NOT_FOUND),
@@ -138,36 +137,29 @@ async fn update_document(
 }
 
 
-async fn delete_document(
-    Extension(pool): Extension<SqlitePool>,
-    Path(id): Path<i64>,
-) -> Result<Json<serde_json::Value>, StatusCode> {
-    let result = sqlx::query!("DELETE FROM documents WHERE id = $1", id)
-        .execute(&pool)
-        .await;
- 
-    match result {
-        Ok(_) => Ok(Json(serde_json::json! ({
-            "message": "Document deleted successfully"
-        }))),
-        Err(_) => Err(StatusCode::NOT_FOUND),
-    }
-}
- 
- 
-async fn create_user(
-    Extension(pool): Extension<SqlitePool>,
-    Json(new_user): Json<CreateUser>,
-) -> Result<Json<User>, StatusCode> {
-    let user = sqlx::query_as!(
-        User,
-        "INSERT INTO users (username, email) VALUES ($1, $2) RETURNING id, username, email",
-        new_user.username,
-        new_user.email
-    )
-    .fetch_one(&pool)
-    .await
-    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
- 
-    Ok(Json(user))
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    // initialize tracing for logging
+    tracing_subscriber::fmt()
+        .with_max_level(Level::INFO)
+        .init();
+
+    dotenv().ok();
+    let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = SqlitePoolOptions::new().connect(&url).await?;
+    info!("Connected to the database!");
+
+    // build our application with a route
+    let app = Router::new()
+    .route("/users", post(create_user))
+    .route("/docs", get(get_document_list).post(create_document))
+    .route("/docs/{id}", get(get_document).put(update_document).delete(delete_document))
+    .layer(Extension(pool));
+
+    // run our app with hyper, listening globally on port 5000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
+    info!("Server is running on http://0.0.0.0:5000");
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
